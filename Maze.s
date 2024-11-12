586bde198b213af0a5dac64c2024c2067a7e2760
Seppe Mestdagh <mestdaghseppe@gmail.com>
2024-11-12 09:29:10 +0100

    Imported some basic staring code and tiles

---
+++ Maze.s
@@ -0,0 +1,53 @@
+; Define PPU Registers
+PPU_CONTROL = $2000
+PPU_MASK = $2001
+PPU_STATUS = $2002
+PPU_SPRRAM_ADDRESS = $2003
+PPU_SPRRAM_IO = $2004
+PPU_VRAM_ADDRESS1 = $2005
+PPU_VRAM_ADDRESS2 = $2006
+PPU_VRAM_IO = $2007
+SPRITE_DMA = $4014
+
+; Gamepad bit values
+PAD_A = $01
+PAD_B = $02
+PAD_SELECT = $04
+PAD_START = $08
+PAD_U = $10
+PAD_D = $20
+PAD_L = $40
+PAD_R = $80
+
+.segment "HEADER"
+INES_MAPPER = 0                                                     ; 0 = NROM
+INES_MIRROR = 0                                                     ; 0 = horizontal mirror/1 = vertical
+INES_SRAM = 0                                                       ; 1 = battery save at $6000-7FFF
+
+.byte 'N', 'E', 'S', $1A ; ID
+.byte $02                                                           ; 16 KB program bank count
+.byte $01                                                           ; 8 KB program bank count
+.byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
+.byte (INES_MAPPER & %11110000)
+.byte $0, $0, $0, $0, $0, $0, $0, $0                                ; padding
+
+.segment "TILES"
+.incbin "Tiles.chr"
+
+.segment "VECTORS"
+.word nmi
+.word reset
+.word irq
+
+.segment "ZEROPAGE"
+
+;*****************************************************************
+; 6502 Zero Page Memory (256 bytes)
+;*****************************************************************
+
+nmi_ready:		.res 1 ; set to 1 to push a PPU frame update, 
+					   ;        2 to turn rendering off next NMI
+gamepad:		.res 1 ; stores the current gamepad values
+
+.segment "OAM"
+oam: .res 256	; sprite OAM data
