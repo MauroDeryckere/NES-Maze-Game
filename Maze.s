4f0fb46a1c4f27ba9c250d841c5c836b85eddde7
Seppe Mestdagh <mestdaghseppe@gmail.com>
2024-11-22 09:30:56 +0100

    changed all lowercase commands to uppercase ones

--- Maze.s
+++ Maze.s
@@ -6,119 +6,119 @@
 ;*****************************************************************
 .segment "CODE"
 .proc wait_frame
-	inc nmi_ready
+	INC nmi_ready
 @loop:
-	lda nmi_ready
-	bne @loop
-	rts
+	LDA nmi_ready
+	BNE @loop
+	RTS
 .endproc
 
 ; ppu_update: waits until next NMI, turns rendering on (if not already), uploads OAM, palette, and nametable update to PPU
 .proc ppu_update
-    lda ppu_ctl0
-	ora #VBLANK_NMI
-	sta ppu_ctl0
-	sta PPU_CONTROL
-	lda ppu_ctl1
-	ora #OBJ_ON|BG_ON
-	sta ppu_ctl1
-	jsr wait_frame
-	rts
+    LDA ppu_ctl0
+	ORA #VBLANK_NMI
+	STA ppu_ctl0
+	STA PPU_CONTROL
+	LDA ppu_ctl1
+	ORA #OBJ_ON|BG_ON
+	STA ppu_ctl1
+	JSR wait_frame
+	RTS
 .endproc
 
 ; ppu_off: waits until next NMI, turns rendering off (now safe to write PPU directly via PPU_VRAM_IO)
 .proc ppu_off
-    jsr wait_frame
-	lda ppu_ctl0
-	and #%01111111
-	sta ppu_ctl0
-	sta PPU_CONTROL
-	lda ppu_ctl1
-	and #%11100001
-	sta ppu_ctl1
-	sta PPU_MASK
-	rts
+    JSR wait_frame
+	LDA ppu_ctl0
+	AND #%01111111
+	STA ppu_ctl0
+	STA PPU_CONTROL
+	LDA ppu_ctl1
+	AND #%11100001
+	STA ppu_ctl1
+	STA PPU_MASK
+	RTS
 .endproc
 
 .segment "CODE"
 .proc reset
-    sei
-    lda #0
-    sta PPU_CONTROL
-    sta PPU_MASK
+    SEI
+    LDA #0
+    STA PPU_CONTROL
+    STA PPU_MASK
     ;sta APU_DM_CONTROL
-    lda #40
-    sta JOYPAD2 
+    LDA #40
+    STA JOYPAD2 
 
-    cld
-    ldx #$FF
-    txs
+    CLD
+    LDX #$FF
+    TXS
 
 wait_vblank:
-    bit PPU_STATUS
-    bpl wait_vblank
+    BIT PPU_STATUS
+    BPL wait_vblank
 
-    lda #0
-    ldx #0
+    LDA #0
+    LDX #0
 
 clear_ram:
-    sta $0000, x
-    sta $0100, x
-    sta $0200, x
-    sta $0300, x
-    sta $0400, x
-    sta $0500, x
-    sta $0600, x
-    sta $0700, x
-    inx
-    bne clear_ram
+    STA $0000, x
+    STA $0100, x
+    STA $0200, x
+    STA $0300, x
+    STA $0400, x
+    STA $0500, x
+    STA $0600, x
+    STA $0700, x
+    INX
+    BNE clear_ram
 
-    lda #255
-    ldx #0
+    LDA #255
+    LDX #0
 
 clear_oam:
-    sta oam, x
-    inx
-    inx
-    inx
-    inx
-    bne clear_oam
+    STA oam, x
+    INX
+    INX
+    INX
+    INX
+    BNE clear_oam
 
 wait_vblank2:
-    bit PPU_STATUS
-    bpl wait_vblank2
+    BIT PPU_STATUS
+    BPL wait_vblank2
 
-    lda #%10001000
-    sta PPU_CONTROL
+    LDA #%10001000
+    STA PPU_CONTROL
 
-    jmp main
+    JMP main
 .endproc
 
 .segment "CODE"
 .proc clear_nametable
-    lda PPU_STATUS 
-    lda #$20
-    sta PPU_VRAM_ADDRESS2
-    lda #$00
-    sta PPU_VRAM_ADDRESS2
+    LDA PPU_STATUS 
+    LDA #$20
+    STA PPU_VRAM_ADDRESS2
+    LDA #$00
+    STA PPU_VRAM_ADDRESS2
 
-    lda #0
-    ldy #30
+    LDA #0
+    LDY #30
     rowloop:
-        ldx #32
+        LDX #32
         columnloop:
-            sta PPU_VRAM_IO
-            dex
-            bne columnloop
-        dey
-        bne rowloop
+            STA PPU_VRAM_IO
+            DEX
+            BNE columnloop
+        DEY
+        BNE rowloop
 
-    ldx #64
+    LDX #64
     loop:
-        sta PPU_VRAM_IO
-        dex
-        bne loop
-    rts
+        STA PPU_VRAM_IO
+        DEX
+        BNE loop
+    RTS
 .endproc
 ;*****************************************************************
 
@@ -127,59 +127,59 @@ wait_vblank2:
 ;*****************************************************************
 .segment "CODE"
 irq:
-	rti
+	RTI
 
 .proc nmi
     ;save registers
-    pha
-    txa
-    pha
-    tya
-    pha
+    PHA
+    TXA
+    PHA
+    TYA
+    PHA
 
-    bit PPU_STATUS
+    BIT PPU_STATUS
 	; transfer sprite OAM data using DMA
-	ldx #0
-	stx PPU_SPRRAM_ADDRESS
-	lda #>oam
-	sta SPRITE_DMA
+	LDX #0
+	STX PPU_SPRRAM_ADDRESS
+	LDA #>oam
+	STA SPRITE_DMA
 
 	; transfer current palette to PPU
-	lda #%10001000 ; set horizontal nametable increment
-	sta PPU_CONTROL 
-	lda PPU_STATUS
-	lda #$3F ; set PPU address to $3F00
-	sta PPU_VRAM_ADDRESS2
-	stx PPU_VRAM_ADDRESS2
-	ldx #0 ; transfer the 32 bytes to VRAM
-	ldx #0 ; transfer the 32 bytes to VRAM
+	LDA #%10001000 ; set horizontal nametable increment
+	STA PPU_CONTROL 
+	LDA PPU_STATUS
+	LDA #$3F ; set PPU address to $3F00
+	STA PPU_VRAM_ADDRESS2
+	STX PPU_VRAM_ADDRESS2
+	LDX #0 ; transfer the 32 bytes to VRAM
+	LDX #0 ; transfer the 32 bytes to VRAM
 @loop:
-	lda palette, x
-	sta PPU_VRAM_IO
-	inx
-	cpx #32
-	bcc @loop
+	LDA palette, x
+	STA PPU_VRAM_IO
+	INX
+	CPX #32
+	BCC @loop
 
 	; write current scroll and control settings
-	lda #0
-	sta PPU_VRAM_ADDRESS1
-	sta PPU_VRAM_ADDRESS1
-	lda ppu_ctl0
-	sta PPU_CONTROL
-	lda ppu_ctl1
-	sta PPU_MASK
+	LDA #0
+	STA PPU_VRAM_ADDRESS1
+	STA PPU_VRAM_ADDRESS1
+	LDA ppu_ctl0
+	STA PPU_CONTROL
+	LDA ppu_ctl1
+	STA PPU_MASK
 
 	; flag PPU update complete
-	ldx #0
-	stx nmi_ready
+	LDX #0
+	STX nmi_ready
 
 	; restore registers and return
-	pla
-	tay
-	pla
-	tax
-	pla
-	rti
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA
+	RTI
 .endproc
 ;*****************************************************************
 
@@ -188,21 +188,21 @@ irq:
 ;*****************************************************************
 .segment "CODE"
 .proc main
-    ldx #0
+    LDX #0
 palette_loop:
-    lda default_palette, x  ;load palettes
-    sta palette, x
-    inx
-    cpx #32
-    bcc palette_loop
+    LDA default_palette, x  ;load palettes
+    STA palette, x
+    INX
+    CPX #32
+    BCC palette_loop
 
-    jsr ppu_off
-    jsr clear_nametable
-    jsr ppu_update
+    JSR ppu_off
+    JSR clear_nametable
+    JSR ppu_update
 mainloop:
-    jsr start
+    JSR start
 
-    jmp mainloop
+    JMP mainloop
 .endproc
 ;*****************************************************************
 
@@ -212,25 +212,25 @@ mainloop:
 .segment "CODE"
 .proc gamepad_poll
 	; strobe the gamepad to latch current button state
-	lda #1
-	sta JOYPAD1
-	lda #0
-	sta JOYPAD1
+	LDA #1
+	STA JOYPAD1
+	LDA #0
+	STA JOYPAD1
 	; read 8 bytes from the interface at $4016
-	ldx #8
+	LDX #8
 loop:
-    pha
-    lda JOYPAD1
+    PHA
+    LDA JOYPAD1
     ; combine low two bits and store in carry bit
-	and #%00000011
-	cmp #%00000001
-	pla
+	AND #%00000011
+	CMP #%00000001
+	PLA
 	; rotate carry into gamepad variable
-	ror
-	dex
-	bne loop
-	sta gamepad
-	rts
+	ROR
+	DEX
+	BNE loop
+	STA gamepad
+	RTA
 .endproc
 ;*****************************************************************
 
@@ -238,10 +238,10 @@ loop:
 ; Start
 ;*****************************************************************
 .proc start
-    jsr gamepad_poll
-    lda gamepad
-    and #PAD_A
-    beq A_NOT_PRESSED
+    JSR gamepad_poll
+    LDA gamepad
+    AND #PAD_A
+    BEQ A_NOT_PRESSED
         ;code for button press here  
         lda a_pressed_last_frame
         bne A_NOT_PRESSED           ;check for pressed this frame
@@ -271,40 +271,40 @@ loop:
 ;*****************************************************************
 .segment "CODE"
 .proc display_map
-    jsr ppu_off
-    jsr clear_nametable
+    JSR ppu_off
+    JSR clear_nametable
 
     JSR run_prims_maze ;temporarily do this here every frame - this is just for resting and will be moved later.
 
     vram_set_address (NAME_TABLE_0_ADDRESS) 
     assign_16i paddr, MAP_BUFFER_ADDRESS    ;load map into ppu
 
-    ldy #0          ;reset value of y
+    LDY #0          ;reset value of y
 loop:
-	lda (paddr),y   ;get byte to load
-    tax
-    lda #8          ;8 bits in a byte
-    sta byte_loop_couter
+	LDA (paddr),y   ;get byte to load
+    TAX
+    LDA #8          ;8 bits in a byte
+    STA byte_loop_couter
 
     byteloop:
-    txa             ;copy x into a to preform actions on a copy
+    TXA             ;copy x into a to preform actions on a copy
     set_Carry_to_highest_bit_A  ;rol sets bit 0 to the value of the carry flag, so we make sure the carry flag is set to the value of bit 7 to rotate correctly
-    rol             ;rotate to get the correct bit on pos 0
-    tax             ;copy current rotation back to x
-    and #%00000001  ;and with 1, to check if tile is filled
-	sta PPU_VRAM_IO ;write to ppu
+    ROL             ;rotate to get the correct bit on pos 0
+    TAX             ;copy current rotation back to x
+    AND #%00000001  ;and with 1, to check if tile is filled
+	STA PPU_VRAM_IO ;write to ppu
 
-    dec byte_loop_couter    ;decrease counter
-    lda byte_loop_couter    ;get value into A
-    bne byteloop            ;repeat byteloop if not done with byte yet
+    DEC byte_loop_couter    ;decrease counter
+    LDA byte_loop_couter    ;get value into A
+    DNE byteloop            ;repeat byteloop if not done with byte yet
 
-    iny
-        cpy #MAP_BUFFER_SIZE              ;the screen is 120 bytes in total, so check if 120 bytes have been displayed to know if we're done
-        bne loop
+    INY
+        CPY #MAP_BUFFER_SIZE              ;the screen is 120 bytes in total, so check if 120 bytes have been displayed to know if we're done
+        BNE loop
 
-    jsr ppu_update
+    JSR ppu_update
 
-    rts
+    RTS
 .endproc
 ;*****************************************************************
 
